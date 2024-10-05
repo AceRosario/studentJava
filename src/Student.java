aca0886eff9ba035daea40400d0e8dbe6f9983c4
AceRosario <haroldace12345@gmail.com>
2024-10-04 18:45:21 -0700

    second commit

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,36 @@
 public class Student {
+    private int ID;
+    private String name;
+    private double[] grades;
 
+    public Student(int ID, String name, double[] grades) {
+        this.ID = ID;
+        this.name = name;
+        this.grades = grades.clone();
+    }
+
+    public String findGrade() {
+        String gradeString;
+        double sum = 0;
+
+        for (int i = 0; i < grades.length; i++) {
+            sum += grades[i];
+        }
+
+        double average = sum / grades.length;
+
+        if (average >= 90.0) {
+            gradeString = "A";
+        } else if (average >= 80.0) {
+            gradeString = "B";
+        } else if (average >= 70.0) {
+            gradeString = "C";
+        } else if (average >= 60.0) {
+            gradeString = "D";
+        } else {
+            gradeString = "F";
+        }
+
+        return gradeString;
+    }
 }
